964786912f5977cc08efde91c15a4353755da222
ts <124226059+uniformization@users.noreply.github.com>
2023-10-09 21:53:06 -0700

    General cleanup and don't depend on Fabric API

--- src/main/java/me/ts/chromaworldborder/ChromaWorldBorder.java
+++ src/main/java/me/ts/chromaworldborder/ChromaWorldBorder.java
@@ -7,7 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ChromaWorldBorder implements ModInitializer {
-    public static final Logger LOGGER = LoggerFactory.getLogger("chromaworldborder");
+	public static final Logger LOGGER = LoggerFactory.getLogger("chromaworldborder");
 
 	public static final Configuration configuration = new Configuration();
 
