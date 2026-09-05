3d345169a64caea97f790338ba95f30e1380cf9a
ts <124226059+uniformization@users.noreply.github.com>
2026-09-05 09:47:02 -0700

    Update to Minecraft 26.2

--- src/main/java/me/ts/chromaworldborder/mixin/MixinWorldBorderRenderer.java
+++ src/main/java/me/ts/chromaworldborder/mixin/MixinWorldBorderRenderer.java
@@ -4,6 +4,7 @@ import me.ts.chromaworldborder.ChromaWorldBorder;
 import me.ts.chromaworldborder.config.Options;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.WorldBorderRenderer;
+import net.minecraft.util.Util;
 import net.minecraft.world.level.border.BorderStatus;
 import net.minecraft.world.level.border.WorldBorder;
 import org.spongepowered.asm.mixin.Mixin;
@@ -17,13 +18,16 @@ import java.awt.*;
 
 @Mixin(WorldBorderRenderer.class)
 public class MixinWorldBorderRenderer {
-    @Unique private Minecraft minecraft;
     @Unique private Options options = null;
 
     @Inject(method = "<init>", at = @At("TAIL"))
     private void onConstructor(CallbackInfo ci) {
-        this.minecraft = Minecraft.getInstance();
         this.options = ChromaWorldBorder.configuration.getOptions();
+    }
+
+    @Unique
+    private float getBorderHue() {
+        return (float) (((Util.getMillis() / 100.0) * this.options.speed) % 360.0);
     }
 
     @Redirect(
@@ -34,9 +38,7 @@ public class MixinWorldBorderRenderer {
         ))
     private int worldBorderColor(BorderStatus instance, WorldBorder border) {
         if (this.options.enabled) {
-            int ticks = ((IMixinLevelRenderer)this.minecraft.levelRenderer).getTicks();
-            float hue = (float) ((ticks * this.options.speed) % 360);
-            return Color.getHSBColor(hue / 360, 1.0f, 1.0f).getRGB();
+            return Color.getHSBColor(getBorderHue() / 360, 1.0f, 1.0f).getRGB();
         }
         return border.getStatus().getColor();
     }
